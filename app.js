0d3913908b8710fe964c75c21c38dbf75b63d357
alexanderfluna <alexanderfluna@outlook.com>
2022-08-12 21:46:31 -0400

    Added javascript

---
+++ app.js
@@ -0,0 +1,36 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".scoreboard");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getCompChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function game(userChoice) {
+    const compChoice = getCompChoice();
+    console.log("userChoice = " + userChoice);
+    console.log("compChoice = " + compChoice);
+}
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("r");
+    })
+    paper_div.addEventListener('click', function() {
+        game("p");
+    })
+    scissors_div.addEventListener('click', function() {
+        game("s");
+    })
+}
+
+main();
+getCompChoice();
