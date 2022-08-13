df1fd1e7a9426d9fe94e0f945a53136ff989f0c6
alexanderfluna <alexanderfluna@outlook.com>
2022-08-13 14:46:04 -0400

    Made all final additions

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let compScore = 0;
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".scoreboard");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+
+function convertToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(userChoice, compChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(compChoice)}. You win!`;
+    userChoice_div.classList.add("green-glow");
+    setTimeout(() => userChoice_div.classList.remove("green-glow"), 500);
+}
+
+function lose(userChoice, compChoice) {
+    compScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML = `${convertToWord(compChoice)} beats ${convertToWord(userChoice)}. You lost. :(`;
+    userChoice_div.classList.add("red-glow");
+    setTimeout(() => userChoice_div.classList.remove("red-glow"), 500);
+}
+
+function draw(userChoice, compChoice) {
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML = `${convertToWord(userChoice)} equals ${convertToWord(compChoice)}. It's a draw.`;
+    userChoice_div.classList.add("brown-glow");
+    setTimeout(() => userChoice_div.classList.remove("brown-glow"), 500);
+}
 
 function getCompChoice() {
     const choices = ['r', 'p', 's'];
@@ -16,21 +50,30 @@ function getCompChoice() {
 
 function game(userChoice) {
     const compChoice = getCompChoice();
-    console.log("userChoice = " + userChoice);
-    console.log("compChoice = " + compChoice);
+
+    switch (userChoice + compChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, compChoice);
+            break;
+        case "sr":
+        case "rp":
+        case "ps":
+            lose(userChoice, compChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, compChoice);
+            break;
+    }
 }
 
 function main() {
-    rock_div.addEventListener('click', function() {
-        game("r");
-    })
-    paper_div.addEventListener('click', function() {
-        game("p");
-    })
-    scissors_div.addEventListener('click', function() {
-        game("s");
-    })
+    rock_div.addEventListener('click', () => game("r"));
+    paper_div.addEventListener('click', () => game("p"));
+    scissors_div.addEventListener('click', () => game("s"));
 }
 
 main();
-getCompChoice();
